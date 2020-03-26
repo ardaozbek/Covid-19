79462f7f55064e41c4972dfac7a4b9bd2c8d1788
Arda OZBEK <ardaozbek@users.noreply.github.com>
2020-03-26 20:29:18 +0300

    Update Covid-19.py

--- Covid-19.py
+++ Covid-19.py
@@ -13,10 +13,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Countries to compare
-Comparison = ['Italy', 'Turkey']
+Comparison = ['Italy', 'Turkey', 'Spain']
 
 # Download raw data
-dfConfirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+dfConfirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 # Drop unused columns
 dfConfirmed.drop(['Province/State', 'Lat', 'Long'], axis=1, inplace=True)
 # Drop rows not in comparison countries
@@ -27,56 +27,86 @@ dfConfirmed = pd.melt(dfConfirmed.groupby('Country/Region').sum().reset_index(),
 dfConfirmed.Date = pd.to_datetime(dfConfirmed.Date, format='%m/%d/%y')
 # Set date index
 dfConfirmed.set_index('Date', inplace=True)
-# 
-dfConfirmedA = dfConfirmed[(dfConfirmed['Country/Region'] == Comparison[1]) & (dfConfirmed['Confirmed'] > 0)].reset_index()
-dfConfirmedA.rename(columns={'Confirmed':'Confirmed'+Comparison[1]}, inplace=True)
-dfConfirmedB = dfConfirmed[(dfConfirmed['Country/Region'] == Comparison[0]) & (dfConfirmed['Confirmed'] > 0)].reset_index()
-dfConfirmedB.rename(columns={'Confirmed':'Confirmed'+Comparison[0]}, inplace=True)
-dfConfirmed = pd.merge(dfConfirmedA, dfConfirmedB, left_index=True, right_index=True, how='outer')
-dfConfirmed.drop(['Date_x', 'Date_y','Country/Region_x','Country/Region_y'], axis=1, inplace=True)
 
-dfDead = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
+# Initialize loop-bucket dataframes
+dfTemp = pd.DataFrame()
+dfCases = pd.DataFrame()
+
+# Pick records with selected countries and confirmed cases more than a certain amount
+for i in Comparison:
+	dfTemp = dfConfirmed[(dfConfirmed['Country/Region'] == i) & (dfConfirmed['Confirmed'] > 95)].reset_index()
+	dfTemp.rename(columns={'Confirmed':i}, inplace=True)
+	dfTemp.drop(['Date', 'Country/Region'], axis=1, inplace=True)
+	dfCases = pd.merge(dfCases, dfTemp, left_index=True, right_index=True, how='outer')
+
+del dfConfirmed, dfTemp
+
+dfDead = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 dfDead.drop(['Province/State', 'Lat', 'Long'], axis=1, inplace=True)
 dfDead = dfDead[dfDead['Country/Region'].isin(Comparison)]
 dfDead = pd.melt(dfDead.groupby('Country/Region').sum().reset_index(), id_vars=['Country/Region'], var_name='Date', value_name='Dead')
 dfDead.Date = pd.to_datetime(dfDead.Date, format='%m/%d/%y')
 dfDead.set_index('Date', inplace=True)
-dfDeadA = dfDead[(dfDead['Country/Region'] == Comparison[1]) & (dfDead['Dead'] > 0)].reset_index()
-dfDeadA.rename(columns={'Dead':'Dead'+Comparison[1]}, inplace=True)
-dfDeadB = dfDead[(dfDead['Country/Region'] == Comparison[0]) & (dfDead['Dead'] > 0)].reset_index()
-dfDeadB.rename(columns={'Dead':'Dead'+Comparison[0]}, inplace=True)
-dfDead = pd.merge(dfDeadA, dfDeadB, left_index=True, right_index=True, how='outer')
-dfDead.drop(['Date_x', 'Date_y','Country/Region_x','Country/Region_y'], axis=1, inplace=True)
 
-# dfRecovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
-# dfRecovered.drop(['Province/State', 'Lat', 'Long'], axis=1, inplace=True)
-# dfRecovered = dfRecovered[dfRecovered['Country/Region'].isin(Comparison)]
-# dfRecovered = pd.melt(dfRecovered.groupby('Country/Region').sum().reset_index(), id_vars=['Country/Region'], var_name='Date', value_name='Recovered')
-# dfRecovered.Date = pd.to_datetime(dfRecovered.Date, format='%m/%d/%y')
-# dfRecovered.set_index('Date', inplace=True)
-# dfRecoveredA = dfRecovered[(dfRecovered['Country/Region'] == Comparison[1]) & (dfRecovered['Confirmed'] > 0)].reset_index()
-# dfRecoveredA.rename(columns={'Recovered':'Recovered'+Comparison[1]}, inplace=True)
-# dfRecoveredB = dfRecovered[(dfRecovered['Country/Region'] == Comparison[0]) & (dfRecovered['Confirmed'] > 0)].reset_index()
-# dfRecoveredB.rename(columns={'Recovered':'Recovered'+Comparison[0]}, inplace=True)
-# dfRecovered = pd.merge(dfRecoveredA, dfRecoveredB, left_index=True, right_index=True, how='outer')
-# dfRecovered.drop(['Date_x', 'Date_y','Country/Region_x','Country/Region_y'], axis=1, inplace=True)
+# Initialize loop-bucket dataframes
+dfTemp = pd.DataFrame()
+dfDeath = pd.DataFrame()
 
-dfCovid = pd.merge(dfConfirmed, dfDead, left_index=True, right_index=True, how='outer')
+# Pick records with selected countries and confirmed cases more than a certain amount
+for i in Comparison:
+	dfTemp = dfDead[(dfDead['Country/Region'] == i) & (dfDead['Dead'] > 0)].reset_index()
+	dfTemp.rename(columns={'Dead':i}, inplace=True)
+	dfTemp.drop(['Date', 'Country/Region'], axis=1, inplace=True)
+	dfDeath = pd.merge(dfDeath, dfTemp, left_index=True, right_index=True, how='outer')
+
+del dfDead, dfTemp
+
+dfRecovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+dfRecovered.drop(['Province/State', 'Lat', 'Long'], axis=1, inplace=True)
+dfRecovered = dfRecovered[dfRecovered['Country/Region'].isin(Comparison)]
+dfRecovered = pd.melt(dfRecovered.groupby('Country/Region').sum().reset_index(), id_vars=['Country/Region'], var_name='Date', value_name='Recovered')
+dfRecovered.Date = pd.to_datetime(dfRecovered.Date, format='%m/%d/%Y')
+dfRecovered.set_index('Date', inplace=True)
+
+# Initialize loop-bucket dataframes
+dfTemp = pd.DataFrame()
+dfRecov = pd.DataFrame()
+
+# Pick records with selected countries and confirmed cases more than a certain amount
+for i in Comparison:
+	dfTemp = dfRecovered[(dfRecovered['Country/Region'] == i) & (dfRecovered['Recovered'] > 0)].reset_index()
+	dfTemp.rename(columns={'Recovered':i}, inplace=True)
+	dfTemp.drop(['Date', 'Country/Region'], axis=1, inplace=True)
+	dfRecov = pd.merge(dfRecov, dfTemp, left_index=True, right_index=True, how='outer')
+
+del dfRecovered, dfTemp
 
 plt.style.use('fivethirtyeight')
-plt.subplot(2, 1, 1)
-plt.plot(dfCovid.loc[0:12,['ConfirmedTurkey', 'ConfirmedItaly']])
-plt.xlabel('Days following the first case')
-plt.legend(loc='upper right', labels=['Turkey', 'Italy'])
-plt.ylabel('Confirmed Cases')
-plt.title('Covid-19 Italy - Turkey Comparison')
 
-plt.subplot(2, 1, 2)
-plt.plot(dfCovid.loc[0:12,['DeadTurkey', 'DeadItaly']])
-plt.xlabel('Days following the first mortality')
-plt.legend(loc='upper right', labels=['Turkey', 'Italy'])
-plt.ylabel('Covid-19 Deaths')
-# plt.title('Covid-19 Italy - Turkey Comparison')
+# # Incase an upper bound needed
+UpperBound = sum(~dfCases.Turkey.isnull()) + 2
+# UpperBound = 10
+
+# Plot data
+fig, (ax1, ax2) = plt.subplots(2)
+fig.suptitle('Covid-19 Comparison')
+for i in dfCases:
+	ax1.plot(dfCases.loc[:UpperBound,[i]], label=i)
+ax1.legend(loc='upper right')
+# ax1.set_title('Cases')
+ax1.set(xlabel='Days following 100th Case', ylabel='Cases')
+
+for i in dfDeath:
+	ax2.plot(dfDeath.loc[:UpperBound,[i]], label=i)
+# ax2.set_title('Deaths')
+ax2.set(xlabel='Days following 1st Death', ylabel='Deaths')
+
+# for i in dfRecov:
+# 	plt.plot(dfRecov.loc[:UpperBound,[i]], label=i)
+# plt.legend(loc='upper right')
+# plt.xlabel('Recoveries')
+# plt.ylabel('Covid-19 Recoveries')
 
 plt.tight_layout()
 plt.show()
+
